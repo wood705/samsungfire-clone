421962a6b8ecdb213357e019c8a1167ada132654
wood705 <yek2794@gmail.com>
2025-05-29 12:40:45 +0900

    design: main-left PC화면 영역 작성 완료

---
+++ javascript/sw-banner.js
@@ -0,0 +1,10 @@
+const swiper = new Swiper(".sw-banner", {
+  pagination: {
+    el: ".swiper-pagination",
+    type: "fraction",
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
