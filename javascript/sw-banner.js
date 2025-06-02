c96207e003c8fe71d6ebd5fadf839a04fda58a85
wood705 <yek2794@gmail.com>
2025-06-02 13:02:57 +0900

    헤더 고정 기능, 메인 배너 슬라이드 기능

--- javascript/sw-banner.js
+++ javascript/sw-banner.js
@@ -1,10 +1,37 @@
-const swiper = new Swiper(".sw-banner", {
+const swBanner = new Swiper(".sw-banner", {
+  centeredSlides: true,
+  loop: true,
+  speed: 500,
+  autoplay: {
+    delay: 2500,
+    disableOnInteraction: false,
+  },
   pagination: {
-    el: ".swiper-pagination",
+    el: ".banner-pagination",
     type: "fraction",
+    renderFraction: function (currentClass, totalClass) {
+      return `
+    <span class="${currentClass}"></span>
+    <span class="${totalClass}"></span>`;
+    },
   },
   navigation: {
-    nextEl: ".swiper-button-next",
-    prevEl: ".swiper-button-prev",
+    nextEl: ".next-bnt",
+    prevEl: ".prev-btn",
   },
+});
+
+const playStopBtn = document.querySelector(".play-stop-btn");
+let isStop = false;
+
+playStopBtn.addEventListener("click", () => {
+  if (!isStop) {
+    swBanner.autoplay.stop();
+    playStopBtn.style.backgroundImage = `url("../../images/main/homepage/slider_play.svg")`;
+    isStop = true;
+  } else {
+    swBanner.autoplay.start();
+    playStopBtn.style.backgroundImage = `url("../../images/main/homepage/slider_stop.svg")`;
+    isStop = false;
+  }
 });
---
+++ javascript/swCard.js
@@ -0,0 +1,8 @@
+var swCard = new Swiper(".sw-card", {
+  slidesPerView: 3,
+  spaceBetween: 30,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+});
