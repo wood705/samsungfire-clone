369d002749cdc1cb3d2bc3af4217430e2bdff30c
wood705 <yek2794@gmail.com>
2025-06-05 12:18:07 +0900

    feat: main-left sw-card 영역 스와이퍼 기능 구현

--- javascript/swCard.js
+++ javascript/swCard.js
@@ -1,8 +1,32 @@
 const swCard = new Swiper(".sw-card", {
-  slidesPerView: 3,
+  // 기본값
+  centeredSlides: false,
+  slidesPerView: "auto",
   spaceBetween: 30,
   pagination: {
     el: ".swiper-pagination",
     clickable: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  //  반응형
+  breakpoints: {
+    // window.width >= 0
+    0: {
+      centeredSlides: true,
+      spaceBetween: 20,
+    },
+    // window.width >= 768
+    768: {
+      centeredSlides: false,
+      spaceBetween: 30,
+    },
+    // window.width >= 1220
+    1220: {
+      centeredSlides: false,
+      spaceBetween: 30,
+    },
   },
 });
